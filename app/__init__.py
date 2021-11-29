8205af61f2fd1abb2dc637efb358474bec90e47c
Ayisha Olanrewaju <olanrea@wwu.edu>
2021-11-29 15:47:40 -0800

    added about page

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ import sys
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 Bootstrap(app)
 app.config.from_object(Config)
 
